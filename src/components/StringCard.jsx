aabc03c5522658002f292cb62ec0049cb37d3a7d
KevinJG994 <KevinJG994@gmail.com>
2025-04-04 13:58:33 +0100

    feat: add hover animation in StringCard and animation in open modal

--- src/components/StringCard.jsx
+++ src/components/StringCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const StringCard = ({ stringMethod, onClick }) => {
   return (
     <div
-      className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow cursor-pointer"
+      className="card bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-700 ease-in-out transform hover:scale-105 cursor-pointer hover:bg-primary hover:animate-pulse group"
       onClick={onClick}
     >
       <div className="card-body flex flex-row gap-6 items-center justify-center">
-        <span>
+        <span className='group-hover:text-base-100'>
           ¿Icono?
         </span>
-        <h2 className="card-title text-primary">{stringMethod.title}</h2>
+        <h2 className="card-title text-primary group-hover:text-base-100">{stringMethod.title}</h2>
       </div>
     </div>
   );
